bff0e9c8f089666cc7c4c55af74ecad09d73911e
paulophsj <snoopyphs@gmail.com>
2025-06-30 19:24:24 -0300

    Adicionando vagas no banco

--- src/main/java/br/com/ifpe/conecta_vagas/modelo/vagas/Vagas.java
+++ src/main/java/br/com/ifpe/conecta_vagas/modelo/vagas/Vagas.java
@@ -1,5 +1,49 @@
 package br.com.ifpe.conecta_vagas.modelo.vagas;
 
-public class Vagas {
-    //isso é um teste
+import br.com.ifpe.conecta_vagas.util.entity.EntidadeAuditavel;
+import br.com.ifpe.conecta_vagas.util.enums.vagas.TipoContrato;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "vagas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Vagas extends EntidadeAuditavel {
+
+    @Column(nullable = false, length = 150)
+    private String titulo;
+
+    @Column(nullable = true)
+    private String descricao;
+
+    @Column(nullable = true, length = 1000)
+    private String requisitos;
+
+    @Column(nullable = true, length = 600)
+    private String localizacao;
+
+    @Column(nullable = true)
+    private Double salario;
+
+    @Column(nullable = false)
+    private Boolean ativa;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoContrato tipoContrato;
+
+    @Column(nullable = true, length = 3)
+    private Integer cargaHoraria;
 }
---
+++ src/main/java/br/com/ifpe/conecta_vagas/util/enums/vagas/TipoContrato.java
@@ -0,0 +1,22 @@
+package br.com.ifpe.conecta_vagas.util.enums.vagas;
+
+public enum TipoContrato {
+    CLT("CLT"),
+    PJ("PJ"),
+    FREELANCER("Freelancer"),
+    MEI("MEI"),
+    VOLUNTARIO("Voluntário"),
+    APRENDIZ("Aprendiz"),
+    INDERTEMINADO("Indeterminado"),
+    ESTAGIO("Estágio");
+
+    private final String tipo;
+
+    private TipoContrato(String tipo){
+        this.tipo = tipo;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+}
